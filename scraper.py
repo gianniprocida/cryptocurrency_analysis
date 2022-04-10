2c279cffd1bdd9184ff85058ea82ac750d4f52d4
gianniprocida <54355107+gianniprocida@users.noreply.github.com>
2022-04-10 09:54:57 +0200

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,42 @@
+import os
+import requests
+from bs4 import BeautifulSoup as bs
+import re
+
+
+DOMAIN = "https://www.cryptodatadownload.com"
+url = "https://www.cryptodatadownload.com/data/binance/"
+
+# Load the web page content
+
+r = requests.get(url, verify=False)
+ # Beautiful soup object
+soup = bs(r.content, "html.parser")
+ncsvfiles=int(input("Enter the number of cryptocurrencies you wish to download:"))
+
+files = []
+
+# Parsing for the links (daily prices)
+
+
+for item in soup.find_all('a'):
+ if re.sub(r'\s+','',item.get_text())=='[Daily]':
+    link=item.get('href')
+    name=link.split('/')[-1]
+    files.append(link.split('/'))
+    if len(files) < ncsvfiles:
+     print(f"Downloading {name} ...")
+     with open (name,'wb') as file:
+        try:
+            resp = requests.get(link,
+                                verify=False, timeout=5)
+            file.write(resp.content)
+        except requests.exceptions.ConnectionError:
+            print("Site not reachable",link)
+    else:
+        pass
+print("Download completed")
+
+
+
+
